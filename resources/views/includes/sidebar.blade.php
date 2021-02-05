d4fdf535db986668ddd8f217a370af3c88771cbf
teddinata <teddibreakthelimit@gmail.com>
2021-02-05 14:30:09 +0700

    crudbasic

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,187 @@
+ <!-- Sidebar -->
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <div class="sidebar-brand-icon rotate-n-15">
+        
+      </div>
+      <div class="sidebar-brand-text mx-3">Administrator I-SMS</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+      <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Home</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+      Folder
+    </div>
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#smsmasuk" aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-sms"></i>
+        <span>SMS Masuk</span>
+      </a>
+      <div id="smsmasuk" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Pilihan:</h6>
+          <a class="collapse-item" href="buttons.html">INBOX</a>
+          <a class="collapse-item" href="cards.html">INBOX GAGAL</a>
+        </div>
+      </div>
+    </li>
+     <!-- Nav Item - Pages Collapse Menu -->
+     <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#smskeluar" aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-sms"></i>
+        <span>SMS Keluar</span>
+      </a>
+      <div id="smskeluar" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Pilihan:</h6>
+          <a class="collapse-item" href="buttons.html">OUTBOX GSM</a>
+          <a class="collapse-item" href="cards.html">OUTBOX MASKING</a>
+        </div>
+      </div>
+    </li>
+     <!-- Nav Item - Pages Collapse Menu -->
+     <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#smsterkirim" aria-expanded="true" aria-controls="collapseTwo">
+        <i class="far fa-share-square"></i>
+        <span>SMS Terkirim</span>
+      </a>
+      <div id="smsterkirim" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Pilihan:</h6>
+          <a class="collapse-item" href="buttons.html">SMS Terkirim GSM</a>
+          <a class="collapse-item" href="cards.html">SMS Terkirim Masking</a>
+        </div>
+      </div>
+    </li>
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="far fa-clock"></i>
+        <span>SMS Terjadwal</span></a>
+    </li>
+
+    <!-- Nav Item - Utilities Collapse Menu -->
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#importsms" aria-expanded="true" aria-controls="collapseUtilities">
+        <i class="fas fa-file-import"></i>
+        <span>Import SMS</span>
+      </a>
+      <div id="importsms" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Pilihan:</h6>
+          <a class="collapse-item" href="utilities-color.html">Tabungan</a>
+          <a class="collapse-item" href="utilities-border.html">Deposito</a>
+          <a class="collapse-item" href="utilities-animation.html">Pinjaman</a>
+          
+        </div>
+      </div>
+    </li>
+    
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Heading -->
+    <div class="sidebar-heading">
+      Pengaturan
+    </div>
+
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-sync-alt"></i>
+        <span>Run Service</span></a>
+    </li>
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#kontak" aria-expanded="true" aria-controls="collapsePages">
+        <i class="far fa-address-book"></i>
+        <span>Kontak</span>
+      </a>
+      <div id="kontak" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Pilihan:</h6>
+          <a class="collapse-item" href="login.html">Nasabah</a>
+          <a class="collapse-item" href="register.html">List Kontak</a>
+          <a class="collapse-item" href="forgot-password.html"> Grup Kontak</a>
+         
+        </div>
+      </div>
+    </li>
+
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-print"></i>
+        <span>Laporan</span></a>
+    </li>
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-reply-all"></i>
+        <span>Auto Replay</span></a>
+    </li>
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-puzzle-piece"></i>
+        <span>Template SMS</span></a>
+    </li>
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-users"></i>
+        <span>Data User</span></a>
+    </li>
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-cog"></i>
+        <span>Pengaturan Modem</span></a>
+    </li>
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-database"></i>
+        <span>Database</span></a>
+    </li>
+    <!-- Nav Item - Sms Terjadwal -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-redo"></i>
+        <span>Changelogs</span></a>
+    </li>
+    <!-- Nav Item - Logout -->
+    <li class="nav-item">
+      <a class="nav-link" href="charts.html">
+        <i class="fas fa-power-off"></i>
+        <span>Logout</span></a>
+    </li>
+
+   
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+  </ul>
+  <!-- End of Sidebar -->
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>@yield('title')</title>
+
+ {{-- Style  --}}
+ 
+ @include('includes.login.style')
+
+
+
+</head>
+
+<body class="bg-gradient-primary">
+
+  @yield('content')
+
+  {{-- Script  --}}
+@include('includes.login.script')
+
+</body>
+
+</html>
